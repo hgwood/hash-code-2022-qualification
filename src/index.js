0cd8702064511cc2f4bce00493e3a1a79e5eb284
Hugo Wood <hugo.wood@zenika.com>
2022-02-24 18:36:45 +0100

    🎨 improve starter code

--- src/index.js
+++ src/index.js
@@ -1,2 +1,7 @@
-const debug = require("debug")("app:index");
+const assert = require("assert");
+const debug = require("debug");
+
+assert.ok(process.env.PETIBRUGNON_INPUT_JSON_FILE_PATH);
+
 const input = require(process.env.PETIBRUGNON_INPUT_JSON_FILE_PATH);
+const logger = debug("root");
